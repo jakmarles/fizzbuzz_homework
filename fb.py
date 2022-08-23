2b37c90652756b111e0291c7b35c5deccc638a2f
ilya <jakmarles123@gmail.com>
2022-08-23 23:00:37 +0300

    Made it even more readable

--- fb.py
+++ fb.py
@@ -5,37 +5,44 @@
 
 
 
+#-----------------#
+range 100 > (101)
+for fizzbuzz in range(1, 101):
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print("fizzbuzz")
+        continue
+    elif fizzbuzz % 3 == 0:
+        print("fizz")
+        continue
+    elif fizzbuzz % 5 == 0:
+        print("buzz")
+        continue
+    print(fizzbuzz)
 
-# range 100 > (101)
-# for fizzbuzz in range(1, 101):
-#     if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
-#         print("fizzbuzz")
-#         continue
-#     elif fizzbuzz % 3 == 0:
-#         print("fizz")
-#         continue
-#     elif fizzbuzz % 5 == 0:
-#         print("buzz")
-#         continue
-#     print(fizzbuzz)
 
 
-# Make it one line
+#-----------------#
+
+Make it one line
 for i in range(1, 101): 
     print("Fizz"*(i % 3 == 0)+"Buzz"*(i % 5 == 0) or str(i)) # str(i) required to print the numbers
 
+#-----------------#
 
-#  count = 0
-# while count < 101:
-#     if (count % 5) == 0 and (count % 3) == 0:
-#         print("FizzBuzz")
-#         count +=1
-#     elif (count % 3) == 0:
-#         print ("Fizz")
-#         count +=1
-#     elif (count % 5) == 0:
-#         print ("Buzz")
-#         count +=1
-#     else:
-#         print (count)
-#         count +=1
+
+
+ count = 0
+while count < 101:
+    if (count % 5) == 0 and (count % 3) == 0:
+        print("FizzBuzz")
+        count +=1
+    elif (count % 3) == 0:
+        print ("Fizz")
+        count +=1
+    elif (count % 5) == 0:
+        print ("Buzz")
+        count +=1
+    else:
+        print (count)
+        count +=1
+#-----------------#
